a77cd7eb88b9a498c2e7bf335be0a9f8163644cf
Manasbek <manasbek.mamytov.bj@gmail.com>
2023-06-12 20:08:17 +0300

    do

---
+++ sabak22_weather_app_data/android/app/src/main/kotlin/com/example/sabak22_weather_app_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sabak22_weather_app_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sabak22_weather_app_data/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  dynamic res;
+  fetchData() async {
+    Dio dio = Dio();
+    await Future.delayed(Duration(seconds: 7));
+    res = await dio.get(
+        'https://api.openweathermap.org/data/2.5/weather?q=bishkek,&appid=41aa18abb8974c0ea27098038f6feb1b');
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyHomePage'),
+      ),
+      body: Center(
+        child: res == null ? const CircularProgressIndicator() : Text('$res'),
+      ),
+    );
+  }
+}
